13e57bfd3a6c29d76f5a7efe834d4e83edb10c74
KHumoyunmirzo <127532144+Humoyunmirzo-coder@users.noreply.github.com>
2024-03-15 21:05:20 +0500

    Create Repository and Clean Arcitecture

---
+++ Prodacts/Aplication/Repository/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aplication.Repository
+{
+    public  interface IRepository
+    {
+        Task<bool> CreateAsync(Products products);
+        Task<bool> UpdateAsync(int Id);
+        Task<bool> DeleteAsync(int Id);
+        Task<bool> GetByIdAsync(int Id);
+        Task<IEnumerable<Products>> GetAllAsync();
+    }
+}
